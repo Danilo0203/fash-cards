88a7e784e720daf8cbac6df8a299010e44471089
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-10-26 09:54:08 -0600

    fix: Ajustar el padding en el componente AuthLayout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({
 }) {
   const pathName = usePathname();
   return (
-    <main className="flex min-h-dvh flex-col items-center justify-center bg-gradient-light px-4 dark:bg-gradient-dark sm:px-[10rem] lg:px-[15rem] xl:px-[25rem] 2xl:px-[45rem]">
+    <main className="flex min-h-dvh flex-col items-center justify-center bg-gradient-light px-4 dark:bg-gradient-dark sm:px-[10rem] lg:px-[15rem] xl:px-[25rem] 2xl:px-[30rem]">
       <section className="container rounded-2xl border p-10">
         <h2 className="mb-4 text-center text-4xl font-bold">
           {pathName.includes("login") ? "Iniciar Sesión" : "Registrate"}
